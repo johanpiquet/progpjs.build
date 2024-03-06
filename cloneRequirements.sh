cdab2ce5e30908e0213b0cf0fa8666ec72ec8efd
johanpiquet <johan.piquet@gmail.com>
2024-03-06 17:15:01 +0100

    Remove submodules

---
+++ cloneRequirements.sh
@@ -0,0 +1,9 @@
+# The "-b main" option allows tracking the last commit.
+# Without that we are stuck to an old version.
+
+git clone -b main https://github.com/progpjs/httpServer.git ./progpjs.httpServer
+git clone -b main https://github.com/progpjs/modules.git ./progpjs.modules
+git clone -b main https://github.com/progpjs/progpAPI.git ./progpjs.progpAPI
+git clone  -b main https://github.com/progpjs/progpjs.git ./progpjs.progpjs
+git clone -b main https://github.com/progpjs/progpV8Engine.git ./progpjs.progpV8Engine
+git clone -b main https://github.com/progpjs/nodejs.git ./progpjs.nodejs
